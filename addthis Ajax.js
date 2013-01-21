d414e5531316cb71a5a0140825c4e6c577e74a7e
AhmadAbdelNaser <dev.ahmadnaser@gmail.com>
2013-01-22 00:31:15 +0200

    Update addthis Ajax.js

--- addthis Ajax.js
+++ addthis Ajax.js
@@ -24,6 +24,33 @@ echo "";
 }
 ?>
 
+
+$("#socialmedia").click(function(){
+
+    $.getScript('http://s7.addthis.com/js/250/addthis_widget.js#pubid=xxx',function(){
+
+        var add_this_html =
+'<div class="addthis_toolbox addthis_default_style ">'+
+'<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a> '+
+'<a class="addthis_button_tweet"></a>'+        
+'<a class="addthis_counter addthis_pill_style">'+
+'</a>'+ 
+'</div>';
+
+$("#loadhere").html(add_this_html);
+addthis.init();
+    });
+
+    
+    
+<div id="socialmedia"> 
+ Show the Social Media
+</div> 
+
+<div id="loadhere"> 
+
+</div> 
+
 */
 
  		function append_social_plugin(){
